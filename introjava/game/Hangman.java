2363261adde9cc3dd103cfe8b13327fe5abd0345
Aman Kumar <amanstar18ak@gmail.com>
2021-07-05 19:58:29 +0530

    HANGMAN_GAME

---
+++ introjava/game/Hangman.java
@@ -0,0 +1,22 @@
+package game;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aman Sinha
+ */
+public class Hangman 
+{
+    public static void main(String[] args)
+    {
+       Menu mobj = new Menu();
+       mobj.addComponent();
+        
+    }
+    
+}
